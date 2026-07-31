bb631f67daacaed97372646a9fe15c154f92e687
Limittlless <169054987+Limittlless@users.noreply.github.com>
2026-07-31 02:24:31 +0100

    feat: parameterize authentication path in Docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
-if [ -d /app/.wwebjs_auth ]; then
-  chown -R nodeapp:nodejs /app/.wwebjs_auth
+AUTH_PATH="${WHATSAPP_WEB_JS_AUTH_PATH:-/app/.wwebjs_auth}"
+
+if [ -d "$AUTH_PATH" ]; then
+  chown -R nodeapp:nodejs "$AUTH_PATH"
+
+  find "$AUTH_PATH" \
+    \( -name 'SingletonLock' -o -name 'SingletonCookie' -o -name 'SingletonSocket' \) \
+    -exec rm -f {} + 2>/dev/null || true
 fi
 
 exec su-exec nodeapp:nodejs "$@"
